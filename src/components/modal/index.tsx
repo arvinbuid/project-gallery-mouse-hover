a66c51668af87d276b868a0e299ec0055e674299
Arvin Buid <arvinbuid01@gmail.com>
2025-06-08 22:17:23 +0800

    Implement moving the modal with the mouse functionality in Modal component.

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import Image from 'next/image';
 import styles from './style.module.css';
+import gsap from 'gsap';
 
+import { useRef, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 const scaleAnimation = {
@@ -43,9 +47,40 @@ interface ModalProps {
 
 const Modal = ({ modal, projects }: ModalProps) => {
     const { active, index } = modal;
+
+    const modalContainerRef = useRef<HTMLDivElement>(null);
+    const cursorRef = useRef<HTMLDivElement>(null);
+    const cursorLabelRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        // Move container
+        const xMoveContainer = gsap.quickTo(modalContainerRef.current, "left", { duration: 0.8, ease: "power3" });
+        const yMoveContainer = gsap.quickTo(modalContainerRef.current, "top", { duration: 0.8, ease: "power3" })
+
+        // Move cursor
+        const xMoveCursor = gsap.quickTo(cursorRef.current, "left", { duration: 0.5, ease: "power3" })
+        const yMoveCursor = gsap.quickTo(cursorRef.current, "top", { duration: 0.5, ease: "power3" })
+
+        // Move cursor label
+        const xMoveCursorLabel = gsap.quickTo(cursorLabelRef.current, "left", { duration: 0.4, ease: "power3" })
+        const yMoveCursorLabel = gsap.quickTo(cursorLabelRef.current, "top", { duration: 0.4, ease: "power3" })
+
+        // Event listener
+        window.addEventListener("mousemove", (e) => {
+            const { pageX, pageY } = e;
+            xMoveContainer(pageX);
+            yMoveContainer(pageY);
+            xMoveCursor(pageX);
+            yMoveCursor(pageY);
+            xMoveCursorLabel(pageX);
+            yMoveCursorLabel(pageY);
+        })
+    }, [])
+
     return (
         <>
             <motion.div
+                ref={modalContainerRef}
                 variants={scaleAnimation}
                 initial="initial"
                 animate={active ? "enter" : "closed"}
@@ -74,8 +109,8 @@ const Modal = ({ modal, projects }: ModalProps) => {
                     }
                 </div>
             </motion.div>
-            <motion.div className={styles.cursor} variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"}></motion.div>
-            <motion.div className={styles.cursorLabel} variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"}>
+            <motion.div ref={cursorRef} className={styles.cursor} variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"}></motion.div>
+            <motion.div ref={cursorLabelRef} className={styles.cursorLabel} variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"}>
                 View
             </motion.div>
         </>
